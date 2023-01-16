087513c2b2fd997e9a5a14324c29866beae15927
marcel.furs <marcel.furs@VENTUM>
2023-01-16 22:06:33 +0100

    Dodaj pliki projektów.

---
+++ SystemBiblioteczny/Admin_Local.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SystemBiblioteczny
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Admin_Local.xaml
+    /// </summary>
+    public partial class Admin_Local : Window
+    {
+        public Admin_Local()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SystemBiblioteczny/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SystemBiblioteczny
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_Client(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Sign_Client(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Sign_Librarian(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Sign_Admin(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
